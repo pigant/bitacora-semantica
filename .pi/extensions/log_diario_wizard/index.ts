ca346bda3774c977901f028fe353ff7b9c943c15
ci <ci@example.com>
2026-03-27 23:49:22 -0300

    feat: make log_diario_wizard self-contained (move wizard implementation)

--- .pi/extensions/log_diario_wizard/index.ts
+++ .pi/extensions/log_diario_wizard/index.ts
@@ -1,7 +1,6 @@
 import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import registerWizard from '../log_diario/wizard.ts';
+import registerWizard from './wizard_impl.ts';
 
 export default function(pi: ExtensionAPI){
-  // delegate to the wizard implementation
   try{ registerWizard(pi); }catch(e){ /* ignore */ }
 }
